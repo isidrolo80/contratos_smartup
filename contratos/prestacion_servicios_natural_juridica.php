2bf05663e45ecbadafd64b3e6d23c649a94db703
Isidro Lopez <isidrolo80@gmail.com>
2020-08-09 11:02:04 -0500

    ultimos cambios

--- contratos/prestacion_servicios_natural_juridica.php
+++ contratos/prestacion_servicios_natural_juridica.php
@@ -25,35 +25,31 @@
  */
 
 
-//Inicio
-$nombreCiudad =  $_POST['nombreCiudad'];
-$diaDelMes = $_POST['diaDelMes'];
-$mesDelAno =  $_POST['mesDelAno'];
-$anoContrato = $_POST['anoContrato'];
+//Informacion persona juridica
+$nombrePersonaJuridica =  $_POST['nombrePersonaJuridica'];
+$RUC = $_POST['RUC'];
+$representanteLegal =  $_POST['representanteLegal'];
+$identificacionRepresentante = $_POST['identificacionRepresentante'];
+$numeroIdentificacionRepresentante = $_POST['numeroIdentificacionRepresentante'];
 
-//Comparecientes
-$nombreVendedor =  $_POST['nombreVendedor'];
-$numeroIdentificacionVendedor =  $_POST['numeroIdentificacionVendedor'];
-$nombreComprador =  $_POST['nombreComprador'];
-$numeroIdentificacionComprador =  $_POST['numeroIdentificacionComprador'];
+//Infomracion persona natural
+$nombrePersonaNatural =  $_POST['nombrePersonaNatural'];
+$identificacionPersonaNatural =  $_POST['identificacionPersonaNatural'];
+$numeroIdentificacionPersonaNatural =  $_POST['numeroIdentificacionPersonaNatural'];
 
-//Antecedentes
-$marca =  $_POST['marca'];
-$modelo =  $_POST['modelo'];
-$anoVehiculo =  $_POST['anoVehiculo'];
-$motor =  $_POST['motor'];
-$chasis =  $_POST['chasis'];
-$placas =  $_POST['placas'];
-$color =  $_POST['color'];
-
-
-//Precio
-$montoEnLetras =  $_POST['montoEnLetras'];
-$montoEnNumeros =  $_POST['montoEnNumeros'];
-
+//Objeto
+$nombreServicioProfesional =  $_POST['nombreServicioProfesional'];
 
 //Forma de pago
-$formaDePago =  $_POST['formaDePago'];
+$montoEnLetras =  $_POST['montoEnLetras'];
+$montoEnNumeros =  $_POST['montoEnNumeros'];
+$numeroDiasPago =  $_POST['numeroDiasPago'];
+$plazoContrato =  $_POST['plazoContrato'];
+
+
+//Datos finales
+$ciudad =  $_POST['ciudad'];
+$fecha =  $_POST['fecha'];
 
 
 // Include the main TCPDF library (search for installation path).
@@ -106,113 +102,95 @@ $pdf->AddPage();
 // set font
 $pdf->SetFont('helvetica', 'B', 20);
 
-//$pdf->Write(0, 'CONTRATO DE COMPRA VENTA VEHÍCULO', '', 0, 'L', true, 0, false, false, 0);
+$pdf->Write(0, 'CONTRATO DE SERVICIOS PROFESIONALES', '', 0, 'L', true, 0, false, false, 0);
 
 
 // Instanciation of inherited class
 
 
 // create some HTML content
-$html = '<style type="text/css">
-	
-h1 { font-family: Garamond; font-size: 1px; font-style: normal; font-variant: normal; font-weight: 400; line-height: 0px; } h3 { font-family: Garamond, Baskerville, "Baskerville Old Face", "Hoefler Text", "Times New Roman", serif; font-size: 14px; font-style: normal; font-variant: normal; font-weight: bold; line-height: 15px; } p { font-family: Garamond, Baskerville, "Baskerville Old Face", "Hoefler Text", "Times New Roman", serif; font-size: 14px; font-style: normal; font-variant: normal; font-weight: 400; line-height: 20px; } blockquote { font-family: Garamond, Baskerville, "Baskerville Old Face", "Hoefler Text", "Times New Roman", serif; font-size: 21px; font-style: normal; font-variant: normal; font-weight: 400; line-height: 30px; } pre { font-family: Garamond, Baskerville, "Baskerville Old Face", "Hoefler Text", "Times New Roman", serif; font-size: 13px; font-style: normal; font-variant: normal; font-weight: 400; line-height: 18px; }
-	
-</style>
-<h2 style="text-align: center;">CONTRATO DE COMPRAVENTA DE VEH&Iacute;CULO</h2>
-<p style="padding-left: 30px;">En la ciudad de '.$nombreCiudad.', a los '.$diaDelMes.' d&iacute;as del mes de '.$mesDelAno.' de '.$anoContrato.', las partes de forma libre y voluntaria suscriben el presente contrato:</p>
-<h3 style="text-align: center;">I. COMPARECIENTES</h3>
-<ol style="text-align: justify;">
-<li>
-<p>Por una parte, '.$nombreVendedor.', por sus propios y personales derechos a quien para efectos del presente contrato se le denominar&aacute; &ldquo;VENDEDOR&rdquo;, y;</p>
-</li>
-<li>
-<p>Por otra parte, '.$nombreComprador.' por sus propios y personales derechos, a quien para los efectos del presente contrato se le denominar&aacute; &ldquo;COMPRADOR&rdquo;.</p>
-</li>
+$html = '<p><strong>CONTRATO DE SERVICIOS PROFESIONALES</strong></p>
+<ol>
+<li><strong>CL&Aacute;USULA PRIMERA: GENERALIDADES. &ndash;</strong></li>
 </ol>
-<h3 style="text-align: center;">II. ANTECEDENTES</h3>
-<ol style="text-align: justify;">
-<li>
-<p>EL VENDEDOR, es propietario del veh&iacute;culo de las siguientes caracter&iacute;sticas:</p>
-<ol style="list-style-type: lower-alpha;">
-<li>
-<p>Marca: '.$marca.'</p> 
-</li>
-<li>
-<p>Modelo: '.$modelo.'</p> 
-</li>
-<li>
-<p>A&ntilde;o: '.$anoVehiculo.'</p> 
-</li>
-<li>
-<p>Motor N&ordm;: '.$motor.'</p> 
-</li>
-<li>
-<p>Chasis: '.$chasis.'</p> 
-</li>
-<li>
-<p>Placas: '.$placas.'</p> 
-</li>
-<li>
-<p>Color: '.$color.'</p> 
-</li>
+<p>Comparecen a la celebraci&oacute;n de este Contrato, libre y voluntariamente:</p>
+<ul>
+<li>Por una parte, '.$nombrePersonaJuridica.' con n&uacute;mero de RUC '.$RUC.', mediante su (Representante Legal) '.$representanteLegal.', con '.$identificacionRepresentante.' n&uacute;mero '.$numeroIdentificacionRepresentante.' (quien adelante y para efectos de este contrato se lo conocer&aacute; como Parte Contratante).</li>
+
+<li>Por otra parte, <strong>'.$nombrePersonaNatural.'</strong> con n&uacute;mero '.$identificacionPersonaNatural.' (quien en adelante y para efectos de este contrato se lo conocer&aacute; como Parte Contratada).</li>
+</ul>
+<p>Los comparecientes son capaces para obligarse y contratar, intervienen en las calidades se&ntilde;aladas, libre y voluntariamente, convienen en celebrar el presente Contrato de Servicios Profesionales.</p>
+<ol start="2">
+<li><strong>CL&Aacute;USULA SEGUNDA: OBJETO. &ndash; </strong></li>
 </ol>
-</li>
-<li>
-<p>EL VENDEDOR declara que sobre el veh&iacute;culo de su propiedad no pesa gravamen alguno, prohibici&oacute;n de enajenar.</p>
-</li>
-<li>
-<p>EL VENDEDOR declara que el veh&iacute;culo se encuentra en perfecto estado y que peri&oacute;dicamente ha sido sujeto de mantenimientos y chequeos mec&aacute;nicos para su &oacute;ptimo funcionamiento.</p>
-</li>
+<p>2.1.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; El objeto de este contrato es que la Parte Contratada preste sus servicios profesionales consistentes en '.$nombreServicioProfesional.' para la Parte Contratante.</p>
+<ol start="3">
+<li><strong>CL&Aacute;USULA TERCERA: OBLIGACIONES DEL CONTRATADO. &ndash; </strong></li>
 </ol>
-<h3 style="text-align: center;">III. OBJETO</h3>
-<ol style="text-align: justify;">
-<li>
-<p>Las partes de com&uacute;n acuerdo y por su convenir a sus intereses celebran el presente contrato de compraventa de veh&iacute;culo.</p>
-</li>
-<li>
-<p>EL VENDEDOR da en venta y perpetua enajenaci&oacute;n a favor de EL COMPRADOR, el veh&iacute;culo descrito en la cl&aacute;usula II del presente contrato.</p>
-</li>
-<li>
-<p>EL VENDEDOR se compromete a efectuar la entrega de toda la documentaci&oacute;n inherente al veh&iacute;culo para efectos del traspaso de dominio.</p>
-</li>
+<p><strong>&nbsp;</strong></p>
+<ul>
+<li>Cumplir el objeto contractual a plena satisfacci&oacute;n de la parte contratante.</li>
+</ul>
+<p>&nbsp;</p>
+<ul>
+<li>Utilizar todos los medios a su alcance para satisfacer la necesidad de la parte contratante.</li>
+<li>Reportar los avances del servicio profesional otorgado.</li>
+<li>Dar correcci&oacute;n o soluci&oacute;n de cualquier posible inconveniente que tenga lugar en la implementaci&oacute;n del servicio prestado.</li>
+</ul>
+<ol start="4">
+<li><strong>CL&Aacute;SULA CUARTA: OBLIGACIONES DEL CONTRATADO. - </strong></li>
 </ol>
-<h3 style="text-align: center;">IV. DECLARACI&Oacute;N</h3>
-<ol style="text-align: justify;">
-<li>
-<p>EL COMPRADOR declara que ha revisado el veh&iacute;culo, recibi&eacute;ndolo en perfecto estado y condiciones, renunciando a reclamos por vicios redhibitorios.</p>
-</li>
+<p>4.1.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Cancelar los honorarios profesionales del Contratado en la forma establecida en la Cl&aacute;usula Quinta de este contrato.</p>
+<p>4.2.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Entregar toda la informaci&oacute;n solicitada por el Contratado.</p>
+<ol start="5">
+<li>C<strong>L&Aacute;USULA QUINTA: FORMA DE PAGO. - </strong></li>
 </ol>
-<h3 style="text-align: center;">V. PRECIO</h3>
-<ol style="text-align: justify;">
-<li>
-<p>El precio del objeto del presente contrato es de '.$montoEnLetras.' D&Oacute;LARES DE LOS ESTADOS UNIDOS DE AM&Eacute;RICA (USD $ '.$montoEnNumeros.') que EL COMPRADOR entrega AL VENDEDOR en '.$formaDePago.' y a su entera satisfacci&oacute;n.</p>
-</li>
+<p>5.1.<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong>Las partes acuerdan que el honorario por la realizaci&oacute;n de los servicios aqu&iacute; descritos se fija en '.$montoEnLetras.' de los Estados Unidos de Am&eacute;rica '.$montoEnNumeros.'.&nbsp; Que ser&aacute;n cancelados '.$numeroDiasPago.' días despu&eacute;s de presentada la factura.</p>
+<ol start="6">
+<li><strong>CL&Aacute;USULA SEXTA: PLAZO. - </strong></li>
 </ol>
-<h3 style="text-align: center;">VI. RATIFICACI&Oacute;N</h3>
-<ol style="text-align: justify;">
-<li>
-<p style="text-align: justify;">Las partes aceptan la vigencia y validez del presente instrumento por contener estipulaciones hechas en sus mutuos beneficios e intereses y en la buena fe de que se cumplir&aacute;n cada una con las obligaciones asumidas, para constancia de lo cual y fiel cumplimiento de lo estipulado, firman en dos ejemplares de igual tenor y valor.</p>
-</li>
+<p>6.1.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; El plazo del contrato se estipula en '.$plazoContrato.' meses, pudiendo quedar autom&aacute;ticamente prorrogado si las partes t&aacute;citamente contin&uacute;an con su relaci&oacute;n contractual.</p>
+<ol start="7">
+<li><strong>CL&Aacute;SULA SEPTIMA: RELACI&Oacute;N CIVIL ENTRE LAS PARTES. &ndash;</strong></li>
 </ol>
+<p>7.1.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; El presente Contrato es de naturaleza civil y mercantil, sujeto al derecho com&uacute;n y singularmente a las reglas del Contrato Civil y Mercantil.</p>
+<p>7.2.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Por tanto, entre el Contratante y el Contratado no existe relaci&oacute;n laboral o de dependencia alguna, ni, consecuentemente sometimiento al C&oacute;digo del Trabajo y Leyes del Seguro Social Obligatorio. No hay subordinaci&oacute;n jur&iacute;dica ni de ninguna otra especie que pudiera significar relaci&oacute;n laboral entre las partes o sus dependientes, puesto que la Parte Contratada prestar&aacute; sus servicios a la Parte Contratante de manera libre, independiente y aut&oacute;noma.</p>
+<ol start="8">
+<li><strong>CL&Aacute;USULA OCTAVA: TERMINACI&Oacute;N ANTICIPADA. &ndash; </strong></li>
+</ol>
+<p>8.1.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Si cualquiera de las partes incurre en incumplimiento debidamente comprobado de sus obligaciones contractuales, y no hubiera remediado dicho incumplimiento despu&eacute;s de un t&eacute;rmino de diez (10) d&iacute;as desde que dicha remediaci&oacute;n fue requerida por escrito, la PARTE afectada, que s&iacute; hubiera cumplido con sus obligaciones, podr&aacute; dar por terminado el Contrato de manera unilateral e inmediata.</p>
+<p>&nbsp;</p>
+<p>8.2.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Si el incumplimiento de cualquiera de las PARTES ocasionar&aacute; a la otra PARTE dificultades imprevistas para el cumplimiento de sus obligaciones, la PARTE afectada, que s&iacute; hubiera cumplido con sus obligaciones, podr&aacute; dar por terminado el Contrato de manera unilateral e inmediata.</p>
+<ol start="9">
+<li><strong>Cl&aacute;usula Novena: Ratificaci&oacute;n. &ndash; </strong></li>
+</ol>
+<p>9.1.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Las partes, completamente conscientes de los alcances y efectos legales de este contrato y de que en su celebraci&oacute;n no ha sido lesi&oacute;n, dolo, violencia, error ni ning&uacute;n otro vicio de la voluntad que pudiera afectar su existencia o validez, se ratifican en todas y cada una de las estipulaciones constantes en las cl&aacute;usulas precedentes, en la ciudad de '.$ciudad.' el '.$fecha.'.</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
-<p>&nbsp;</p>
-<table style="width: 567px; margin-left: auto; margin-right: auto;">
+<table>
 <tbody>
-<tr style="height: 83px;">
-<td style="height: 83px; width: 278.5px;">
-<h3 style="padding-left: 60px; text-align: center;">'.$nombreVendedor.'</h3>
+<tr>
+<td width="283">
+<p>PARTE CONTRATANTE</p>
 </td>
-<td style="height: 83px; width: 278.5px;">
-<h3 style="text-align: center;">'.$nombreComprador.'</h3>
+<td width="283">
+<p>PARTE CONTRATADA</p>
 </td>
 </tr>
-<tr style="height: 2px;">
-<td style="height: 2px; width: 278.5px;">
-<h3 style="text-align: center; padding-left: 60px;">CI: '.$numeroIdentificacionVendedor.'</h3>
+<tr>
+<td width="283">
+<p>'.$nombrePersonaJuridica.'</p>
 </td>
-<td style="height: 56.15625px; width: 278.5px;">
-<h3 style="text-align: center; padding-left: 60px;">CI: '.$numeroIdentificacionComprador.'</h3>
+<td width="283">
+<p><strong>'.$nombrePersonaNatural.'</strong></p>
+</td>
+</tr>
+<tr>
+<td width="283">
+<p>'.$representanteLegal.'</p>
+</td>
+<td width="283">
+<p><strong>&nbsp;</strong></p>
 </td>
 </tr>
 </tbody>
